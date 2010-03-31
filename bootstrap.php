962f1f4a5f53a7bf61038c4862f9016868c87eb8
Eduard Kracmar <kracmar@dannax.sk>
2010-03-31 14:43:33 +0200

    RobotLoader moved to config.ini

--- bootstrap.php
+++ bootstrap.php
@@ -6,12 +6,6 @@ define('LIBS_DIR', realpath(APP_DIR.'/../libs'));
 
 require_once LIBS_DIR .'/Nette/loader.php';
 
-$robot = new RobotLoader();
-$robot->addDirectory(APP_DIR);
-$robot->addDirectory(LIBS_DIR);
-$robot->autoRebuild = true;
-$robot->register();
-
 Debug::enable();
 
 Environment::loadConfig();
